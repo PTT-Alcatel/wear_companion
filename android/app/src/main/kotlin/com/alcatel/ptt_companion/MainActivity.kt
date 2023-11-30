e8ca9a18048513bd0757fae387704299e5a9d480
bastianfbr <badmaxyt@gmail.com>
2023-11-30 22:34:17 +0100

    fix: :bug: fix square screen by rendering without background

--- android/app/src/main/kotlin/com/alcatel/ptt_companion/MainActivity.kt
+++ android/app/src/main/kotlin/com/alcatel/ptt_companion/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.alcatel.ptt_companion
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+
 }
